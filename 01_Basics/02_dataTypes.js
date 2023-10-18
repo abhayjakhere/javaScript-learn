00b809702b5646d4aba03f985ad3860e585889e4
Abhay Jakhere <126489320+abhayjakhere@users.noreply.github.com>
2023-10-18 11:10:34 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version of 
+// alert(3+3); // we are using node js not browser. it is use to send pop. In broweser console we can use it
+
+let name = "Abhay"
+let age = 20
+let isLoggedIn = false
+
+/*
+    Data types
+    1. number -> 2 to power 53
+    if exceed then use bigint
+    2. string -> written in ""
+    3. boolean -> true or false , 0/1 and its type is object
+    4. null -> standalone value (refers to null or nothing.empty) it doesnt have any type as it is undefined
+    5. undefined -> declare but value is not defined
+    6. symbol -> used for uniqueness
+*/
